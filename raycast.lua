b6039588de72fc602ccb51916b5026c89dec6ef5
AZ9tumas <77965895+AZ9tumas@users.noreply.github.com>
2022-09-07 19:43:42 +0400

    Update raycast.lua

--- raycast.lua
+++ raycast.lua
@@ -1,22 +1,37 @@
 local module = {}
 
+local light_direction = workspace.light.Position
+
 function module:CastRay(x,y)
 	
-	local finalx = x
-	local finalz = y
+	local finalx = x--module.startXpos - i
+	local finalz = y--module.startZpos + j
 	
 	local params = RaycastParams.new()
 	params.FilterType = Enum.RaycastFilterType.Blacklist
-	params.FilterDescendantsInstances = {workspace.SpawnLocation}
-	params.IgnoreWater = true
+	params.FilterDescendantsInstances = {workspace.light}
+	params.IgnoreWater = false
 	
 	local rayResult = workspace:Raycast(Vector3.new(finalx, 100, finalz), Vector3.new(0, -1000, 0), params)
-	
-	if rayResult and rayResult.Instance and rayResult.Instance:IsA("BasePart") then
-		--print(x, y)
-		return rayResult.Instance
+	if rayResult and rayResult.Instance then
+		
+		-- cast another ray and check the lighting
+		local lightRayCast = workspace:Raycast(rayResult.Position, light_direction, params)
+		
+		local color
+		
+		if rayResult.Instance:IsA("BasePart") and rayResult.Instance ~= workspace.Terrain then
+			color = rayResult.Instance.BrickColor.Color
+		elseif rayResult.Instance == workspace.Terrain then
+			-- terrain work
+			local terrainType = rayResult.Material
+			color = terrainType == Enum.Material.Water and workspace.Terrain.WaterColor or workspace.Terrain:GetMaterialColor(terrainType)
+			--print('terrain color: ', color)
+		end
+		
+		return {Color = color, Name = rayResult.Instance.Name, InShade = lightRayCast ~= nil}
 	else
-		return {Color = Color3.new(0, 0.560784, 0.839216), Name = 'NAn'}
+		return {Color = Color3.new(0, 0.560784, 0.839216), Name = 'NAn', InShade = false}
 	end
 end
 
